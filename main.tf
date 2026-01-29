7b3830d26dc61d505d9f2eddd3ef0fdaf6dc799f
Okorieokwy <okorie.okwy@gmail.com>
2026-01-29 10:56:06 +0100

    Add S3 bucket resource with website configuration

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
-provider "aws" {
+resource "aws_s3_bucket" "example" {
+  bucket = "starttech-backend-2026-01-29-xyz11"
   region = "us-east-1"
-}
 
-resource "aws_s3_bucket" "frontend" {
-  bucket = "starttech-frontend-yourname"
+  website {
+    index_document = "index.html"
+  }
+
+  # Modern AWS: ACLs disabled
+  # access_control = "private"   # optional, ignored if ACLs are off
 }
